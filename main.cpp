7c17e53cd8bf869662550f4aacfe96247b328469
Сергей Васильков <sergeyvasilkov23@gmail.com>
2020-05-06 13:30:26 +0600

    fix pwd

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <cstdlib>
+#include <unistd.h>
 
 using namespace std;
 
@@ -10,74 +11,10 @@ static bool equalsStats(struct stat s1, struct stat s2){
     return s1.st_dev == s2.st_dev && s1.st_ino == s2.st_ino;
 }
 
-string pwd()
-{
+string pwd() {
     string resultPath = string();
-
-    struct stat rootstat{};
-
-    if (lstat("/", &rootstat) == -1)
-    {
-        return nullptr;
-    }
-
-    string currpath;
-    currpath += ".";
-
-    while (true)
-    {
-        struct stat currstat{};
-        if (lstat(currpath.c_str(), &currstat) == -1)
-        {
-            return nullptr;
-        }
-
-        if (equalsStats(rootstat, currstat)) { //проверка дошли ли мы до rootstat
-            break;
-        }
-
-        string upDir;
-        upDir = "../" + currpath ;
-
-        DIR *dir = opendir(upDir.c_str());
-
-        if (dir == nullptr) //проверка вдруг opendir не смогла открыть папку из за прав доступа и т.п.
-        {
-            cerr << "error: opendir " << upDir << endl << "result path: " << resultPath.c_str() << endl;
-            return nullptr;
-        }
-
-        dirent *de;
-        while ((de = readdir(dir)) != nullptr)
-        {
-            //так как мы будем идти по всем файлам этой директории, то скипаем точку и точку точку
-            if ((strcmp(de->d_name, ".")) == 0) continue;
-            if ((strcmp(de->d_name, "..")) == 0) continue;
-
-            string depath;
-            depath += upDir;
-            depath += "/";
-            depath += de->d_name;
-
-            struct stat destat{};
-
-            if (lstat(depath.c_str() , &destat) == -1)
-            {
-                cerr << "error: lstat for " << depath << endl;
-                return nullptr;
-            }
-
-            if (equalsStats(destat, currstat)) {
-                string tmp;
-                tmp+="/";
-                tmp+=de->d_name;
-                resultPath = tmp + resultPath;
-                break;
-            }
-        }
-        closedir(dir);
-        currpath = "../"+ currpath;
-    }
+    char *buffer = new char[1024];
+    resultPath = string(getwd(buffer));
     return resultPath;
 }
 
